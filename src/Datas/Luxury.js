c21189e34611388c37ee5bcabdfd7d848a7d7c53
shaheryar <shaheryarsherry114@gmail.com>
2024-07-02 06:16:57 +1100

    second commit

--- src/Datas/Luxury.js
+++ src/Datas/Luxury.js
@@ -1,87 +1,74 @@
 const LuxuryRoom = [
     {
       id: 23,
-      title: 'Young genius',
-      price: "400",
+      title: 'Imperial GoldSuite Room',
+      price: "18,000",
       image: ""
     },
     {
       id:24,
-      title: 'Young genius Grey',
-      price: "199",
+      title: 'Royal Retreat Room',
+      price: "18,000",
       image: "/Assets/images (17).jpg",
       category: "WOMENS PRODUCTS"
     },
     {
       id: 25,
-      title: 'Young genius White',
-      price:" 299",
+      title: 'Presidential Suite Room',
+      price:" 18,000",
       image: "/Assets/images (18).jpg",
       category: "WOMENS PRODUCTS"
     },
     {
       id: 26,
-      title: "Young genius Black",
-      price:" 95",
+      title: "Grand Palace Room",
+      price:" 18,000",
       image: "/Assets/il_fullxfull.3518088952_f82j.jpg",
       category: "WOMENS PRODUCTS"
     },
     {
       id: 27,
-      title: "Young genius Brown",
-      price: "93",
+      title: "Elite Haven Room",
+      price: "18,000",
       image: "/Assets/mockup-63363b1a.webp",
       category: "WOMENS PRODUCTS"
     },
     {
       id: 28,
-      title: 'Diamond Pendant',
-      price: "499",
+      title: 'Luxe Lounge Room',
+      price: "18,000",
       image: "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg",
       "category": "WOMENS PRODUCTS"
     },
     {
       id: 29,
-      title: 'Gold Hoop Earrings',
-      price: "199",
+      title: 'The Penthouse Room',
+      price: "18,000",
       image: "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg",
       category: "WOMENS PRODUCTS"
     },
     {
       id: 30,
-      title: 'Sapphire Bracelet',
-      price:" 299",
+      title: 'Prestige Suite Room',
+      price:" 18,000",
       image: "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg",
       category: "WOMENS PRODUCTS"
     },
     {
       id: 31,
-      title: "MBJ Women's Solid Short Sleeve Boat Neck V",
-      price:" 95",
+      title: "Grandiose Getaway Room",
+      price:" 18,000",
       image: "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg",
       category: "WOMENS PRODUCTS"
     },
     {
       id: 32,
-      title: "MBJ Women's Solid Short Sleeve Boat Neck V",
-      price: "93",
+      title: "Luxe Lavish Room",
+      price: "18,000",
       image: "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg",
       category: "WOMENS PRODUCTS"
     },
-    {
-      id: 33,
-      title: 'Diamond Pendant',
-      price: "499",
-      image: "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg",
-      category: "WOMENS PRODUCTS"
-    },
-    {
-      id: 34,
-      title: 'Gold Hoop Earrings',
-      price: "199",
-      image: "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg",
-      category: "WOMENS PRODUCTS"
-    },
+ 
   ];
   
   export default LuxuryRoom;
